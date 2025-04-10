8a947a2954a414e18efcd63159d311d443f751f9
Andrew Siminiuс <44125451+SimAndrew@users.noreply.github.com>
2025-04-10 20:15:45 +0300

    feat: add nextPerson, prevPerson functionality

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,52 @@ function App() {
 	const [index, setIndex] = useState(0);
 	const { name, job, image, text } = people[index];
 
+	const checkNumber = (number) => {
+		if (number > people.length - 1) {
+			return 0;
+		}
+		if (number < 0) {
+			return people.length - 1;
+		}
+		return number;
+	};
+
+	const nextPerson = () => {
+		setIndex((currentIndex) => {
+			const newIndex = currentIndex + 1;
+			return checkNumber(newIndex);
+		});
+	};
+
+	const prevPerson = () => {
+		setIndex((currentIndex) => {
+			const newIndex = currentIndex - 1;
+			return checkNumber(newIndex);
+		});
+	};
+
 	return (
 		<main>
 			<article className="review">
 				<div className="img-container">
 					<img src={image} alt={name} className="person-img" />
-					<span className="quote-icon"><FaQuoteRight/></span>
+					<span className="quote-icon">
+						<FaQuoteRight />
+					</span>
 				</div>
 
-				<h4 className='author'>{name}</h4>
-				<p className='job'>{job}</p>
-				<p className='info'>{text}</p>
+				<h4 className="author">{name}</h4>
+				<p className="job">{job}</p>
+				<p className="info">{text}</p>
+
+				<div className="btn-container">
+					<button className="prev-btn" onClick={prevPerson}>
+						<FaChevronLeft />
+					</button>
+					<button className="next-btn" onClick={nextPerson}>
+						<FaChevronRight />
+					</button>
+				</div>
 			</article>
 		</main>
 	);
