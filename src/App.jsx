527fd585198f1d19e4472c3bc9da9d5389e262b8
Andrew Siminiuс <44125451+SimAndrew@users.noreply.github.com>
2025-04-10 20:39:53 +0300

    feat: add randomPerson functionality

--- src/App.jsx
+++ src/App.jsx
@@ -31,6 +31,14 @@ function App() {
 		});
 	};
 
+	const randomPerson = () => {
+		let randomNumber = Math.floor(Math.random() * people.length);
+		if (randomNumber === index) {
+			randomNumber = index + 1;
+		}
+		setIndex(checkNumber(randomNumber));
+	};
+
 	return (
 		<main>
 			<article className="review">
@@ -53,6 +61,10 @@ function App() {
 						<FaChevronRight />
 					</button>
 				</div>
+
+				<button className="btn btn-hipster" onClick={randomPerson}>
+					Surprise Me
+				</button>
 			</article>
 		</main>
 	);
